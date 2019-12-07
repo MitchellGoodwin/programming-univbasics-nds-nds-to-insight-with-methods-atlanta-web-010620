d9356be285fb09a5e0651574d9ed9a38bdfc6be2
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-07 21:34:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,11 +7,18 @@ require 'directors_database'
 
 def directors_totals(nds)
   result = {}
+  
   nil
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  director_earnings = 0 
+  inner_index = 0 
+  while inner_index < director_data.length do
+    director_earnings += director_data[inner_index][:worldwide_gross]
+    inner_index += 1 
+  end
+  return director_earnings
 end
